4763761dd0df87b349f12adc653dd639fa8adffc
DU Xiaoming <BP367@win.iwi.local>
2017-06-20 17:21:36 +0200

    -

--- app/component/About.jsx
+++ app/component/About.jsx
@@ -5,9 +5,13 @@ import React from 'react';
 class About extends React.Component {
 
     render() {
+
+        const { match, location, history } = this.props
+
         return (
             <div>
                 <p> About page </p>
+                <p>{JSON.stringify(match)}</p>
             </div>
         )
     }
